7a2297e6a78e67703cdfaeb18c797ed310098a40
Zero <joyh0808@mgail.com>
2024-01-08 15:59:06 +0900

    Add: MVVM용 그룹 생성

---
+++ OwnStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OwnStudy
+//
+//  Created by 조영현 on 1/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
